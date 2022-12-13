f64eb70f3a88bde330bcdb4e8a106239dc6eb829
eniiazov <esenniiazov@gmail.com>
2022-12-12 20:10:01 -0600

    some changes

--- src/esenReviews/Game.java
+++ src/esenReviews/Game.java
@@ -6,22 +6,25 @@ import java.util.Scanner;
 public class Game {
 
     public static void main(String[] args) {
-        Random random = new Random();
-        int someRandom = random.nextInt(11);
+        for (int i = 0; i < 5; i++){
+            // code
+            Random random = new Random();
+            int someRandom = random.nextInt(11);
 
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Please guess the number");
-        int guess = scanner.nextInt();
+            Scanner scanner = new Scanner(System.in);
+            System.out.println("Please guess the number");
+            int guess = scanner.nextInt();
 
-        if (guess > someRandom){
-            System.out.println("Your guess is too high");
+            if (guess > someRandom){
+                System.out.println("Your guess is too high");
+            }
+            else if(guess < someRandom){
+                System.out.println("Your guess is too low");
+            }
+            else {
+                System.out.println("Congratulations!");
+            }
+            System.out.println("Random number was: " + someRandom);
         }
-        else if(guess < someRandom){
-            System.out.println("Your guess iis too low");
-        }
-        else {
-            System.out.println("Congratulations!");
-        }
-        System.out.println("Random number was: " + someRandom);
     }
 }
